03d371c3ac41a0c89a88db6393bb33cfa12972a6
Luca Campana <luca.campa.94@gmail.com>
2025-06-08 23:25:52 +0100

    Add concurrency

--- src/Stress.js
+++ src/Stress.js
@@ -11,23 +11,35 @@ export class Stress {
   async performTest() {
     const fullUrl = `${this.baseUrl}${this.request.path}`;
     console.log(`Starting stress test on ${fullUrl} using ${this.request.verb.toUpperCase()} method.`);
+
     let iteration = 0;
+
     while (this.config.count === -1 || iteration < this.config.count) {
-      iteration++;
-      console.log(`Iteration ${iteration}: Sending request...`);
-      try {
-        const response = await axios({
-          method: this.request.verb,
-          url: fullUrl,
-          headers: this.request.headers,
-          data: this.request.data
-        });
-        console.log(`Iteration ${iteration}: Received status ${response.status}`);
-      } catch (err) {
-        console.error(`Iteration ${iteration}: Request failed with error: ${err.message}`);
+      const requestPromises = [];
+      for (let c = 0; c < this.config.concurrency; c++) {
+        iteration++;
+        if (this.config.count !== -1 && iteration > this.config.count) break;
+        requestPromises.push(this._sendRequest(iteration, fullUrl));
       }
+      await Promise.all(requestPromises);
       await sleep(this.config.interval);
     }
+
     console.log('Stress test complete.');
+  }
+
+  async _sendRequest(iteration, fullUrl) {
+    console.log(`Iteration ${iteration}: Sending request...`);
+    try {
+      const response = await axios({
+        method: this.request.verb,
+        url: fullUrl,
+        headers: this.request.headers,
+        data: this.request.data,
+      });
+      console.log(`Iteration ${iteration}: Received status ${response.status}`);
+    } catch (err) {
+      console.error(`Iteration ${iteration}: Request failed with error: ${err.message}`);
+    }
   }
 }
--- src/StressConfiguration.js
+++ src/StressConfiguration.js
@@ -6,14 +6,18 @@ export class StressConfiguration {
     } catch (error) {
       throw new Error(`Invalid JSON in configuration file: ${error.message}`);
     }
-    const { count, interval } = parsed;
+    const { count, interval, concurrency } = parsed;
     if (typeof count !== 'number') {
       throw new Error('Configuration file missing valid property: count');
     }
     if (typeof interval !== 'number') {
       throw new Error('Configuration file missing valid property: interval');
     }
+    if (concurrency && typeof concurrency !== 'number') {
+      throw new Error('Configuration file invalid property: concurrency');
+    }
     this.count = count;
     this.interval = interval;
+    this.concurrency = concurrency || 1
   }
 }
